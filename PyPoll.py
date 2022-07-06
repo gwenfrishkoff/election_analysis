3947afd4341ed1377ce7f864985a17e7887d11fe
gwenfrishkoff <gfrishkoff@gmail.com>
2022-07-06 13:09:59 -0700

    changed file locations

---
+++ PyPoll.py
@@ -0,0 +1,11 @@
+### GOAL: Generate vote count report that will be used to certify US congressional race
+
+### Step 1: Open  data file
+
+### Step 2: Retrieve names of all candidates
+
+### Step 3: Add vote count for each candidate
+
+### Step 4: Get the total votes for each candidate
+
+### Step 5: Get the total votes cast for the election
