32c4d4b56abfb9f01563c924e3b78ede76847bde
gwenfrishkoff <gfrishkoff@gmail.com>
2022-07-06 17:28:09 -0700

    Adding the election audit code

--- PyPoll.py
+++ PyPoll.py
@@ -10,10 +10,10 @@ file_to_load = os.path.join("resources", "election_results.csv")
 # Assign variable to save analyses to file and add to path
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-
-## Add vote count for each candidate
-# Initialize a total vote counter
+# Initialize total & winning vote counters
 total_votes = 0
+winning_count = 0
+winning_percentage = 0
 
 ## Declare list and dict objects
 candidate_options = []  #create empty list object
@@ -33,6 +33,8 @@ with open(file_to_load) as election_data:
     
     # Print each row in CSV file
     for row in file_reader:
+
+## Add vote count for each candidate
         
         # Add to the total vote count
         total_votes += 1
@@ -67,14 +69,23 @@ for candidate_name in candidate_votes:
 
     # Determine winning vote count and candidate
     # Determine if the votes is greater than the winning count
-    #if (votes > winning_count) and (vote_percentage > winning_percentage):
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
          # If true then set winning_count = votes and winning_percent =
          # vote_percentage.
-    #     winning_count = votes
-    #     winning_percentage = vote_percentage
+         winning_count = votes
+         winning_percentage = vote_percentage
          # And, set the winning_candidate equal to the candidate's name.
-    #     winning_candidate = candidate_name
+         winning_candidate = candidate_name
 
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+
+    print(winning_candidate_summary)
+    
 ###  To do: print out the winning candidate, vote count and percentage to terminal
 
     # Print the candidate name and percentage of votes
